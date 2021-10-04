6595d3779d5996753e6248186549c83ee67d22a4
Jemima Khoo <jemima.khoo98@gmail.com>
2021-10-04 21:39:15 +1100

    added in simple form for guidance for sheng, with basic validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 import Navigation from "./Components/Navigation";
 import Page from "./Components/Page";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [age, setAge] = useState("");
+
   return (
     <div>
       <Navigation />
-      <Page />
+      <Page
+        fname={fname}
+        setFname={setFname}
+        lname={lname}
+        setLname={setLname}
+        age={age}
+        setAge={setAge}
+      />
     </div>
   );
 }
--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -1,9 +1,52 @@
-function Page() {
+import { Form, Button } from "react-bootstrap";
+
+function Page(props) {
+  const changeFname = (e) => {
+    props.setFname(e.target.value);
+  };
+  const changeLname = (e) => {
+    props.setLname(e.target.value);
+  };
+  const changeAge = (e) => {
+    props.setAge(e.target.value);
+  };
+
+  const isDisabled = () => {
+    if (
+      props.fname.trim() === "" ||
+      props.lname.trim() === "" ||
+      props.age.trim() === ""
+    ) {
+      return true;
+    }
+    return false;
+  };
   return (
     <div className="d-flex justify-content-around">
-      <div>One third</div>
-      <div>Two Third</div>
-      <div>Three Third</div>
+      <Form.Group>
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" value={props.fname} onChange={changeFname} />
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" value={props.lname} onChange={changeLname} />
+        <Form.Label>Age</Form.Label>
+        <Form.Control type="text" value={props.age} onChange={changeAge} />
+        <br />
+        <Button
+          onClick={() =>
+            console.log(
+              "first name is: ",
+              props.fname,
+              " last name is: ",
+              props.lname,
+              " age is: ",
+              props.age
+            )
+          }
+          disabled={isDisabled()}
+        >
+          Submit button
+        </Button>
+      </Form.Group>
     </div>
   );
 }
